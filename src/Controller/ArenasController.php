7fc15b0f9adfcf54e742dbf8837728c9097e4a6c
Juan Pablo Vielma <Juan Pablo Vielma>
2016-11-07 15:02:52 +0100

    Database setup

---
+++ src/Controller/ArenasController.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Controller;
+use App\Controller\AppController;
+/**
+* Personal Controller
+* User personal interface
+*
+*/
+class ArenasController  extends AppController
+{
+	public function toto()
+	{
+	// die('test');
+	}
+
+    public function index()
+    {
+    	$this->set('myname', "JP Vielma");
+    	$this->loadModel('Fighters');
+		$figterlist=$this->Fighters->find('all');
+		pr($figterlist->toArray());
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function fighter()
+    {
+
+    }
+
+    public function sight()
+    {
+
+    }
+
+    public function diary()
+    {
+
+    }
+}
