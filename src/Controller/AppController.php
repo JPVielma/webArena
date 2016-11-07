98cd7f57f2d5b94b2255bdb5a7bc0c803071a934
Juan Pablo Vielma <Juan Pablo Vielma>
2016-11-07 16:48:17 +0100

    Auth Failed, controller and Models for fighters guilds and players

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -43,6 +43,22 @@ class AppController extends Controller
 
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
+        // $this->loadComponent('Auth', [
+        //     'authenticate'=> [
+        //         'Basic' => ['userModel' => 'Players'],
+        //         'Form'=> [
+        //                 'userModel'=>'Players',
+        //                 'fields'=> [
+        //                     'username'=> 'email',
+        //                     'password'=> 'password'               
+        //                 ]
+        //         ]
+        //     ],
+        //     'loginAction'=>[
+        //         'controller'=>'Players',
+        //         'action' => 'login'
+        //     ]
+        // ]);
     }
 
     /**
